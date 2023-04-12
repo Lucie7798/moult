67383fd0569adc7e022202b37e90a3298121b5f4
Lucie7798 <lucie.guinebert@outlook.com>
2023-04-12 02:32:48 +0200

    Add Category + ProductCrud

--- src/DataFixtures/GenderFixtures.php
+++ src/DataFixtures/GenderFixtures.php
@@ -8,17 +8,31 @@ use Doctrine\Persistence\ObjectManager;
 
 class GenderFixtures extends Fixture
 {
+    public const GENDER_HOMME = 'gender_homme';
+    public const GENDER_FEMME = 'gender_femme';
+
     public function load(ObjectManager $manager): void
     {
+        // Création de l'entité 'Homme'
         $gender = new Gender();
         $gender->setName('Homme');
+        $manager->persist($gender);
+        $this->addReference(self::GENDER_HOMME, $gender);
 
+        // Création de l'entité 'Femme'
         $gender = new Gender();
         $gender->setName('Femme');
-
         $manager->persist($gender);
-        $this->addReference('Homme', $gender);
+        $this->addReference(self::GENDER_FEMME, $gender);
 
         $manager->flush();
     }
+
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+    
 }
