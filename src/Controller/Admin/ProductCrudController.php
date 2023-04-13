bbb56a80a50647c9cb2f723648207369c658661b
Lucie7798 <lucie.guinebert@outlook.com>
2023-04-13 17:10:09 +0200

    Add filters ProductCrud

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use App\Form\ItemImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
@@ -19,13 +20,22 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('name')
+            ->add('price')
+            ->add('category')
+        ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         yield FormField::addTab('Info générales')->setIcon('fas fa-info');
         yield Field::new('name');
         yield AssociationField::new('category')->setRequired(false);
         yield AssociationField::new('gender')->setRequired(false);
-        yield TextEditorField ::new('description');
+        yield TextEditorField ::new('description')->hideOnIndex();
         yield MoneyField::new('price')->setStoredAsCents(false)->setCurrency('EUR');
 
         yield FormField::addTab('Images')->setIcon('fas fa-images');
