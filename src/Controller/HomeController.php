2c9f8eb2fcefdfde9ac6297ea7795c49309ea36e
Lucie7798 <lucie.guinebert@outlook.com>
2023-05-01 17:57:12 +0200

    Config fichier Card

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\HeaderRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(HeaderRepository $headerRepository): Response
+    public function index(HeaderRepository $headerRepository, ProductRepository $productRepository): Response
     {
         $header = $headerRepository->findActiveHeader();
-        
+
+        // Récupérer les produits homme et femme
+        $menProducts = $productRepository->findBy(['gender' => 'homme']); // Remplacez 'homme' par la valeur appropriée pour les produits homme
+        $womenProducts = $productRepository->findBy(['gender' => 'femme']); // Remplacez 'femme' par la valeur appropriée pour les produits femme
+
         return $this->render('home/index.html.twig', [
             'header' => $header,
+            'menProducts' => $menProducts,
+            'womenProducts' => $womenProducts,
         ]);
     }
 }
